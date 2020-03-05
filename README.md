1b4b4a54d327544d3e8af17666a29aee28f1eb2b
weliu <liuwenzhe2008@qq.com>
2020-03-05 18:36:55 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Prepare data like this:
+```
+$ mkdir -p ~/.uniq/5
+$ mkdir -p ~/.uniq/6
+```
+Then create two text file inside the above folder:
+```
+$ ls ~/.uniq/5/
+all.txt   sign.txt
+```
+
---
+++ main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path"
+	"strings"
+)
+
+var home string
+
+func init() {
+	var err error
+	home, err = os.UserHomeDir()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	printAbsent("5")
+	printAbsent("6")
+}
+
+func printAbsent(cls string) {
+	names, err := readLineToSlice(getFilePath(cls, "all.txt"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(names)
+	uniq := sliceToSet(names)
+
+	signs, err := readLineToSlice(getFilePath(cls, "sign.txt"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(signs)
+
+	for _, s := range signs {
+		for u := range uniq {
+			if strings.Contains(s, u) {
+				delete(uniq, u)
+				break
+			}
+		}
+	}
+	fmt.Printf("\n---------- Absent in class %s -----------\n", cls)
+	fmtUniq(uniq)
+}
+
+func getFilePath(cls string, name string) string {
+	return path.Join(home, ".uniq", cls, name)
+}
+
+func readLineToSlice(filePath string) (names []string, err error) {
+	f, err := os.Open(filePath)
+	if err != nil {
+		return
+	}
+	defer f.Close()
+	names = make([]string, 0, 64)
+	bf := bufio.NewReader(f)
+	for {
+		var line string
+		line, err = bf.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				err = nil
+			}
+			return
+		}
+		line = strings.TrimSpace(line)
+		if line == "" {
+			continue
+		}
+		names = append(names, line)
+	}
+	return
+}
+
+func sliceToSet(s []string) map[string]bool {
+	m := make(map[string]bool)
+	for _, it := range s {
+		m[it] = true
+	}
+	return m
+}
+
+func setToSlice(m map[string]bool) []string {
+	a := make([]string, len(m))
+	i := 0
+	for k := range m {
+		a[i] = k
+		i++
+	}
+	return a
+}
+
+func fmtUniq(m map[string]bool) {
+	for k := range m {
+		fmt.Println(k)
+	}
+}
